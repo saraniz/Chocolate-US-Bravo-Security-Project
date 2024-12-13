2afb6b229fd67feee3b46847ff6d9a547af15fd2
kishgi <kishgi1234@gmail.com>
2024-12-13 20:41:58 +0530

    Added models and DB connection and updated .env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config(); 
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+connectDB();
+
+app.use(express.json()); 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
