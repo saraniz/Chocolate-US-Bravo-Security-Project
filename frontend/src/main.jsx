5ef2de234d54dafeb5a987d18694da2a85433b21
Naveena-27 <rajendran9naveena@gmail.com>
2025-01-04 21:27:01 +0530

    Login page code uploaded

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Footer />
+    <LoginPage/>
   </StrictMode>,
 )
