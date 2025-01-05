e7c38eda822a9ca1211780fd8e7bd3f0eb8f32f9
Naveena-27 <rajendran9naveena@gmail.com>
2025-01-05 12:36:07 +0530

    bgbg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import LoginPage from './pages/LoginPage.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import HomePage from './pages/HomePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage/>
-  </StrictMode>,
-)
+    {/* Uncomment the page you want to render */}
+    {/* <LoginPage /> */}
+    <HomePage />
+  </StrictMode>
+);
