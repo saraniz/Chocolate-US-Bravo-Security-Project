00a7ea92bcf9c36e6970bd8d02cc362231885fc2
kajanthann <148801608+kajanthann@users.noreply.github.com>
2025-01-07 12:49:39 +0530

    updated button.jsx and add validation.js

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({placeholder}) => {
   return (
     <div>
-      
+      <button className='btn px-5 text-white' id='btnn'>{placeholder}</button>
     </div>
   )
 }
